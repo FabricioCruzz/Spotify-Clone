393972730771597893c29a894604bbbe6e263372
Fabricio Cruz <fabriciocruzz77@gmail.com>
2022-07-27 22:53:56 -0300

    Aula-05

--- src/app/components/painel-esquerdo/painel-esquerdo.component.ts
+++ src/app/components/painel-esquerdo/painel-esquerdo.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { faGuitar, faHome, faMusic, faSearch } from '@fortawesome/free-solid-svg-icons';
+import { IPlaylist } from 'src/app/Interfaces/IPlaylist';
+import { SpotifyService } from 'src/app/services/spotify.service';
 
 @Component({
   selector: 'app-painel-esquerdo',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PainelEsquerdoComponent implements OnInit {
 
-  constructor() { }
+  menuSelecionado = 'Home'
+
+  playlists: IPlaylist[] = []
+
+  // Icones
+  homeIcone = faHome
+  pesquisarIcone = faSearch
+  artistaIcone = faGuitar
+  playlistIcone = faMusic
+
+  constructor(
+    private spotifyService: SpotifyService,
+  ) { }
 
   ngOnInit(): void {
+    this.buscarPlaylist()
+  }
+
+  botaoClick(botao: string){
+    this.menuSelecionado = botao
+  }
+
+  async buscarPlaylist(){
+    this.playlists = await this.spotifyService.buscarPlaylistUsuario()
   }
 
 }
